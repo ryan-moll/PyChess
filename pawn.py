b32e3afe36b4345d76f12a601dfbf92b7a9f52e3
ryan-moll <rmoll16@gmail.com>
2020-03-25 21:36:56 -0700

    Finished updateMoves for pawn

--- pawn.py
+++ pawn.py
@@ -1,7 +1,30 @@
 from piece import Piece
 
 class Pawn(Piece):
-    def updateMoves(self):
-        print("I AM PAWN AH")
-        self.availableMoves = ["PAWN MOVEZ"]
-        return 0
+    def updateMoves(self, board):
+        # TODO: Add pawn promotion
+        rank = self.position[1]
+        file = self.position[0]
+        if self.color: # Piece is black
+            if not board[file][rank-1]:
+                self.availableMoves.append([file,rank-1])
+            #TODO: VALIDATE FILE FOR EDGE CASES
+            if board[file+1][rank-1] and board[file+1][rank-1][0] is "W":
+                self.availableMoves.append([file+1][rank-1])
+            if board[file-1][rank-1] and board[file-1][rank-1][0] is "W":
+                self.availableMoves.append([file-1][rank-1])
+            if rank is 6:
+                if not board[file][rank-1] and not board[file][rank-2]:
+                    self.availableMoves.append([file,rank-2])
+        else: # Piece is white
+            if not board[file][rank+1]:
+                self.availableMoves.append([file,rank+1])
+            #TODO: VALIDATE FILE FOR EDGE CASES
+            if board[file+1][rank+1] and board[file+1][rank+1][0] is "B":
+                self.availableMoves.append([file+1][rank+1])
+            if board[file-1][rank+1] and board[file-1][rank+1][0] is "B":
+                self.availableMoves.append([file-1][rank+1])
+            if rank is 1:
+                if not board[file][rank+1] and not board[file][rank+2]:
+                    self.availableMoves.append([file,rank+2])
+        return
--- piece.py
+++ piece.py
@@ -1,12 +1,12 @@
 class Piece:
     def __init__(self, pos, player):
-        self.position = pos
-        self.availableMoves = []
-        self.color = player
+        self.position = pos # List [x,y] where x is the file and y is the rank- e.g. Rook on A1 has position [0,0]
+        self.availableMoves = [] # List of positions [[a,b],[c,d],[e,f]] that the piece can move to from its current position
+        self.color = player # Binary value to represent the player value 0 = White, 1 = black
         self.updateMoves()
 
 
-    def updateMoves(self):
-        print("Never initialized updateMoves() for child class")
-        self.availableMoves = ["AH"]
-        return 0
+    # def updateMoves(self):
+    #     print("Never initialized updateMoves() for child class")
+    #     self.availableMoves = ["AH"]
+    #     return 0
